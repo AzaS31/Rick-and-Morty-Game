35ad9d73de82e2e5cbef830a872e2a6f78394d14
Azamat <azamatsalaly@gmail.com>
2025-09-30 19:22:11 +0500

    project version 1.0.6

--- src/infra/provably-fair/CryptoUtils.js
+++ src/infra/provably-fair/CryptoUtils.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 
 function generateKey() {
-  return crypto.randomBytes(16).toString('hex');
+  return crypto.randomBytes(32).toString('hex');
 }
 
 function generateHMAC(key, value) {
